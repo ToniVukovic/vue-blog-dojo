a036b5e1d3e75af02b4f08d4332d246ffa86e0fa
Toni Vukovic <tonivukovic1204@gmail.com>
2023-06-11 21:32:54 +0200

    add json file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="home">
     <p>Home</p>
-    <div v-if="toggleShow">
+    <div v-if="posts.length">
       <PostList :posts="posts" />
     </div>
-    <button @click="toggleShow = !toggleShow">Show posts</button>
-    <button @click="posts.pop()">delete post</button>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
@@ -17,18 +16,25 @@ export default {
   name: "HomeView",
   components: { PostList },
   setup() {
-    const posts = ref([
-      {
-        title: "welcome to the blog",
-        body: "lorem ipsum Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere officia, molestiae laborum voluptates odit minima quisquam ex, culpa consequuntur architecto id reprehenderit. Itaque eaque ex laboriosam accusamus temporibus sit praesentium omnis facere? Labore iure at delectus suscipit magni incidunt architecto eum sequi voluptates! Omnis atque, dicta recusandae ab molestiae quidem!",
-        id: 1,
-      },
-      { title: "top 5 css tips", body: "lorem ipsum", id: 2 },
-    ]);
+    const posts = ref([]);
+    const error = ref(null);
 
-    const toggleShow = ref(true);
+    const load = async () => {
+      try {
+        let data = await fetch("http://localhost:3000/posts");
+        if (!data.ok) {
+          throw Error("data is not ok");
+        }
+        posts.value = await data.json();
+      } catch (err) {
+        error.value = err.message;
+        console.log(err.message);
+      }
+    };
 
-    return { posts, toggleShow };
+    load();
+
+    return { posts };
   },
 };
 </script>
